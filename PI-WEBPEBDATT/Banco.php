79ecc1d3317db6a1ed16045cde030afbf1c52fae
Enzo <ramhunatra2018@gmail.com>
2024-11-10 17:40:18 -0300

    Login funcionando

--- PI-WEBPEBDATT/Banco.php
+++ PI-WEBPEBDATT/Banco.php
@@ -1,29 +1,14 @@
 <?php
-extract($_POST);
-   
-   if(isset($BT1)) {
-       $incluir = ("INSERT INTO `rotas`(`rota`, `Origem`, `Destino`, `Valor`) VALUES ($id,'$origem','$destino',$valor)");
-       banco("localhost","root",NULL,"eunapolis",$incluir);
-   }
-   if(isset($BT2)) {
-       $update = "UPDATE `rotas` SET `Valor`= $valor WHERE `Origem` = '$origem' and `Destino` = '$destino'";
-      banco("localhost","root",NULL,"eunapolis",$update);
-      echo $update;
-   }
+    $user = 'root';
+    $senha = '';
+    $db = 'pinovo';
+    $host = 'localhost';
+
+    $banco = new mysqli($host, $user, $senha, $db);
+
+    if($banco->connect_errno){
+        echo "Falha ao fazer a conexão de dados".$banco->error;
+    } else {
+        echo "Conexão efetuada com sucesso!";
+    }
 ?>
-<html>
-    <head>
-        
-    </head>
-    <body>
-        <Form action = "Banco.php" method = post>
-             ID da rota: <input type = "text" name = "id"> <br/>
-             Origem: <input type = "text" name = "origem"> <br/>
-             Destino: <input type = "text" name = "destino"> <br/>
-             Valor: <input type = "text" name = "valor"> <br/>
-             <input type = "submit" value = "Salvar" name = "BT1">
-             <input type = "submit" value = "Atualizar" name = "BT2">
-            
-        </Form>
-    </body>
-</html>
